ef5fd9ead55acb512c14a0b90c6ce711f6fdb09a
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-08-02 17:31:25 +0200

    embellish /lister

--- src/main/java/dev/sgp/listener/InitAppListener.java
+++ src/main/java/dev/sgp/listener/InitAppListener.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import dev.sgp.entite.Collaborateur;
+import dev.sgp.entite.Departement;
 import dev.sgp.exception.NirDontMatchException;
 import dev.sgp.service.CollaborateurService;
 import dev.sgp.service.DepartementService;
@@ -24,13 +25,17 @@ public class InitAppListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		List<Collaborateur> collabs = initCollaborateurs();
-
 		for (Collaborateur c : collabs) {
 			try {
 				collabService.sauvegarderCollaborateur(c);
 			} catch (NirDontMatchException e) {
 				e.printStackTrace();
 			}
+		}
+		
+		List<Departement> departements = initDepartements();
+		for(Departement d : departements) {
+			departService.sauvegarderDepartement(d);
 		}
 	}
 
@@ -42,11 +47,22 @@ public class InitAppListener implements ServletContextListener {
 	private List<Collaborateur> initCollaborateurs() {
 		List<Collaborateur> list = new ArrayList<>();
 		list.add(new Collaborateur("1", "Albert", "Dupont", LocalDate.of(1993, 8, 26), "46 de la Boustifaille",
-				"123456789123456", "mail@mail.com", "cheminDeLimage.jpg", ZonedDateTime.now(), true));
+				"123456789123456", "mail@mail.com", "cheminDeLimage.jpg", ZonedDateTime.now(), true, new Departement(1, "Informatique"), "Développeur"));
 		for (int i = 0; i < 10; i++) {
 			list.add(new Collaborateur("m" + i, "nom" + i, "prenom" + i, LocalDate.of(i, 8, 26), "adresse" + i,
-					"123456789123456", "mail" + i, "cheminPhoto" + i + ".jpg", ZonedDateTime.now(), true));
+					"123456789123456", "mail" + i, "cheminPhoto" + i + ".jpg", ZonedDateTime.now(), true, new Departement(i, "Comptabilité"), "Comptable"));
 		}
+		return list;
+	}
+	
+	private List<Departement> initDepartements() {
+		List<Departement> list = new ArrayList<>();
+		
+		list.add(new Departement(1, "Comptabilité"));
+		list.add(new Departement(2, "Ressources Humaines"));
+		list.add(new Departement(3, "Informatique"));
+		list.add(new Departement(4, "Administratif"));
+		
 		return list;
 	}
 }
--- src/main/webapp/WEB-INF/views/collab/listerCollaborateurs.jsp
+++ src/main/webapp/WEB-INF/views/collab/listerCollaborateurs.jsp
@@ -9,12 +9,48 @@
 	<link rel="stylesheet" href="<c:url value ='/bootstrap-3.3.7-dist/css/bootstrap.css'></c:url>">
 </head>
 <body>
-	<h1>Liste des collaborateurs</h1>
+	<h1>Les collaborateurs</h1>
 	
-	<ul class="list-group">
-		<c:forEach var="col" items="${listeCollabs}">
-			<li class="list-group-item">${col.nom} ${col.prenom} ${col.emailPro}</li>
-		</c:forEach>
-	</ul>
+<!-- 	<ul class="list-group"> -->
+<%-- 		<c:forEach var="col" items="${listeCollabs}"> --%>
+<%-- 			<li class="list-group-item">${col.nom} ${col.prenom} ${col.emailPro}</li> --%>
+<%-- 		</c:forEach> --%>
+<!-- 	</ul> -->
+
+
+	<div class="container">
+		<div class="row">
+		
+			<c:forEach var="col" items="${listeCollabs}">
+			
+				<div class="col-md-4">
+					<div class="thumbnail">
+						<h3>${col.nom} ${col.prenom}</h2>
+						<hr>
+						<div class="row">
+							<div class="col-md-4">
+								<img src="" width="64px" height="64px">					
+							</div>
+							<div class="col-md-4">
+								<p>Fonction</p>		
+								<p>Département</p>		
+								<p>Email</p>		
+								<p>Adresse</p>		
+							</div>
+							<div class="col-md-4">
+								<p>${col.intitulePoste}</p>							
+								<p>${col.departement.nom}</p>							
+								<p>${col.emailPro}</p>							
+								<p>${col.adresse}</p>		
+								<a href="#" class="btn btn-default">Éditer</a>				
+							</div>
+						</div>
+					</div>
+				</div>
+				
+			</c:forEach>
+			
+		</div>
+	</div>
 </body>
 </html>
